7dae341680075525e8b7b1af6dd854839a418b20
Hägers git <erik_hager@hotmail.com>
2020-05-10 22:31:44 +0200

    My first routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Fragment } from "react";
-import "./components/layout/Landing.js";
-import "./components/layout/Navbar.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import { Navbar } from "./components/layout/Navbar.js";
 import { Landing } from "./components/layout/Landing.js";
+import { Register } from "./components/auth/Register";
+import { Login } from "./components/auth/Login";
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
